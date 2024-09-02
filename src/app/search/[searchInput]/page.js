3876079e0fb85fde4f38b4edc810d2ea5f669d0c
Donut <punnatat_ng@cmu.ac.th>
2024-09-02 23:07:45 +0700

    not done

--- src/app/search/[searchInput]/page.js
+++ src/app/search/[searchInput]/page.js
@@ -1,12 +1,13 @@
 "use client";
+import { movieDB } from "@lib/movieDB";
+import MovieRow from "@components/MovieRow";
 
-import { MovieRow } from "@/components/MovieRow";
-import { movieDB } from "@/libs/movieDB";
-
-export default function SearchResultPage() {
+export default function SearchResultPage({params}) {
+  const searchInput = String(params);
+  
   //tip1 : before filtering movie, replace all "%20" with " " (space) in the input
   // const processedSearchInput = ...
-
+  const processedSearchInput = searchInput.replaceAll("%20"," ");
   /*
   tip2 : Use "includes" string method to check substring
   Example : "ABC".includes("AB") -> return true
@@ -17,14 +18,32 @@ export default function SearchResultPage() {
     you code here...
   );
   */
+ const filteredMovies = movieDB.filter((movie)=>
+    movie.title.toLocaleLowerCase().includes(processedSearchInput.toLocaleLowerCase())
+)
 
   return (
     <div>
       <p className="fw-bold fs-4 text-center my-0">
-        Searching &quot; ... &quot;
+        Searching &quot; {processedSearchInput} &quot;
       </p>
-      <p className="fw-bold fs-4 text-center">Found ... result(s)</p>
+      <p className="fw-bold fs-4 text-center">Found {filteredMovies.length} result(s)</p>
       {/* Use  "filteredMovies" variable to map-loop rendering MovieRow component */}
+      <div>
+        {filteredMovies.map((movie,i)=>(
+                <MovieRow
+                key = {movie.id}
+                id = {movie.id}
+                title = {movie.title}
+                number={i+1}
+                rating = {movie.rating}
+                
+                />
+
+              ))}
+
+      </div>
+      
     </div>
   );
 }
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,16 @@
 import { footerProps } from "@lib/types";
 
-export default function Footer() {
+export default function Footer(props:{
+  year:string;
+  fullName:string;
+  studentId:string;
+
+}) {
+  
   return (
     <div className="mt-3">
       <hr />
-      <p className="text-secondary text-center">Copyright © TOP-10-IMDB</p>
+      <p className="text-secondary text-center">Copyright © {props.year} {props.fullName} {props.studentId}</p>
     </div>
   );
 }
